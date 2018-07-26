50b9eaf043cba3f224f180b35253803b505ff825
Mark Hermano <hermano.mark.mh@gmail.com>
2018-07-26 10:39:42 +0800

    styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  }
 
   return (
-    <div className="Person" style={style}>
+    <div className={classes.Person}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!! </p>
       <span>{props.children}</span>
       <input type="text" onChange={props.changed} value={props.name} />
