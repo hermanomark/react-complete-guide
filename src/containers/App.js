1b3de7b9a35a19d7c072d162efb171b08827bb03
Mark Hermano <hermano.mark.mh@gmail.com>
2018-08-01 18:06:26 +0800

    higher order components

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -98,7 +99,7 @@ class App extends PureComponent {
     }
 
     return (
-        <WithClass classes={classes.App}>
+        <Aux>
           <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
             <Cockpit 
               appTitle={this.props.title}
@@ -107,7 +108,7 @@ class App extends PureComponent {
               clicked={this.togglePersonsHandler}
             />
           {persons}
-        </WithClass>
+        </Aux>
       );
 
     //Understanding JSX
@@ -116,4 +117,4 @@ class App extends PureComponent {
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
